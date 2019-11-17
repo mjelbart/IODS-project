bdfe15b67f69d32ef0692d07405840af0e626371
craftecode <41639393+craftecode@users.noreply.github.com>
2019-11-17 16:19:00 +0200

    ongoing chapter 3

--- data/create_alc.R
+++ data/create_alc.R
@@ -8,6 +8,7 @@ library(dplyr)
 
 # 3. Reading in the data sets and exploring the stucture and dimensions.
 student_math <- read.table("data/student-mat.csv", header=TRUE, sep=";")
+colnames(student_port)
 student_port <- read.table("data/student-por.csv", header=TRUE, sep=";")
 dim(studentmath)
 str(studentmath)
@@ -18,7 +19,7 @@ str(studentport)
 join_by <- c("school", "sex", "age", "address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery","internet")
 
 student_combo <- inner_join(student_math, student_port, by = join_by, suffix = c(".math", ".port"))
-
+colnames(student_combo)
 # a note on inner_join - return all rows from x where 
 # there are matching values in y, and all columns from x and y. 
 # If there are multiple matches between x and y, all combination of the matches are returned.
@@ -35,6 +36,7 @@ glimpse(student_combo)
 
 # create a new data frame with only the joined columns
 alc <- select(student_combo, one_of(join_by))
+colnames(alc)
 
 # create a new data frame with the columns in the datasets which were not used for joining the data
 notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
@@ -64,8 +66,18 @@ for(column_name in notjoined_columns) {
 glimpse(alc)
 
 
-# 6. Take the average of the answers related to weekday and weekend alcohol consumption
-# to create a new column 'alc_use' to the joined data. 
-# Then use 'alc_use' to create a new logical column 'high_use' 
+# 6a. Take the average of the answers related to weekday and weekend alcohol consumption
+# to create a new column 'alc_use' to the joined data.
+
+alc <- mutate(alc, alc_use = (student_combo$Dalc + student_combo$Walc) / 2)
+
+# 6b. Then use 'alc_use' to create a new logical column 'high_use' 
 # which is TRUE for students for which 'alc_use' is greater than 2 (and FALSE otherwise).
 
+alc <- mutate(alc, high_use = alc_use > 2)
+
+
+# 7. Glimpse at the joined and modified data to make sure everything is in order. 
+# The joined data should now have 382 observations of 35 variables.
+
+glimpse(alc)
