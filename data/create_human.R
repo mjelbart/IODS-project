eeb02fc7963a1005857dc129c13b8e73705b3959
craftecode <41639393+craftecode@users.noreply.github.com>
2019-11-27 13:48:36 +0200

    correction to create_human.R after submission date in prep for week 5.

--- data/create_human.R
+++ data/create_human.R
@@ -14,35 +14,35 @@ summary(gii)
 
 # 4. Look at the meta files and rename the variables with (shorter) descriptive names.
 library(tidyverse)
-hd %>% 
+hd1 <- hd %>% 
   rename(
-    Human.Development.Index..HDI. = hdi,
-    Life.Expectancy.at.Birth = life.expec,
-    Expected.Years.of.Education = expec.edu,
-    Mean.Years.of.Education = mean.edu,
-    Gross.National.Income..GNI..per.Capita = gni,
-    GNI.per.Capita.Rank.Minus.HDI.Rank = gni.hdi
+    hdi = Human.Development.Index..HDI.,
+    life.expec = Life.Expectancy.at.Birth,
+    expec.edu = Expected.Years.of.Education,
+    mean.edu = Mean.Years.of.Education,
+    gni = Gross.National.Income..GNI..per.Capita,
+    gni.hdi = GNI.per.Capita.Rank.Minus.HDI.Rank
   )
 
-gii %>% 
+gii1 <- gii %>% 
   rename(
-    Gender.Inequality.Index..GII. = gii,
-    Maternal.Mortality.Ratio = matmort,
-    Adolescent.Birth.Rate = birth,
-    Percent.Representation.in.Parliament = parliment,
-    Population.with.Secondary.Education..Female. = seced.fem,
-    Population.with.Secondary.Education..Male. = seced.mal,
-    Labour.Force.Participation.Rate..Female. = lfpr.fem,
-    Labour.Force.Participation.Rate..Male. = lfpr.mal
+    gii = Gender.Inequality.Index..GII.,
+    matmort = Maternal.Mortality.Ratio,
+    birth = Adolescent.Birth.Rate,
+    parliment = Percent.Representation.in.Parliament,
+    seced.fem = Population.with.Secondary.Education..Female.,
+    seced.mal = Population.with.Secondary.Education..Male.,
+    lfpr.fem = Labour.Force.Participation.Rate..Female.,
+    lfpr.mal = Labour.Force.Participation.Rate..Male.
   )
-
+summary(gii1)
 
 # 5. Mutate the “Gender inequality” data and create two new variables. 
 # The first one should be the ratio of Female and Male populations with secondary education in each country. (i.e. edu2F / edu2M). 
 # The second new variable should be the ratio of labour force participation of females and males in each country (i.e. labF / labM).
 
-gii <- gii %>% mutate(FemtoMal_edu = seced.fem/seced.mal)
-gii <- gii %>% mutate(FemtoMal_work = lfpr.fem/lfpr.mal)
+gii1 <- gii1 %>% mutate(FemtoMal_edu = seced.fem/seced.mal)
+gii1 <- gii1 %>% mutate(FemtoMal_work = lfpr.fem/lfpr.mal)
 
 
 # 6. Join together the two datasets using the variable Country as the identifier. 
@@ -50,6 +50,7 @@ gii <- gii %>% mutate(FemtoMal_work = lfpr.fem/lfpr.mal)
 # The joined data should have 195 observations and 19 variables. 
 # Call the new joined data "human" and save it in your data folder.
 
-human <- inner_join(hd,gii,by="country")
+human <- inner_join(hd,gii1,by="Country")
 dim(human) 
 write.csv(human, file="data/human.csv", row.names = FALSE)
+summary(read.csv("data/human.csv", sep = ","))
